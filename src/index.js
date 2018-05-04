8b2b907d2e70c43c60a0ac6b815033f79d45460a
Bishal Baaniya <071bct513@pcampus.edu.np>
2018-05-04 18:30:06 +0545

    office push

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 console.log(`I'm a silly entry point`);
+console.log(store.getState());
+store.subscribe(() => console.log('Look ma, Redux!!'));
+store.dispatch( addArticle({ name: 'React Redux Tutorial for Beginners', id: 1 }) )
 
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
@@ -6,3 +9,4 @@ window.iAmJavascriptES6 = iAmJavascriptES6;
 
 import App from "./App";
 import style from "./main.css";
+import index from "./js/index";
